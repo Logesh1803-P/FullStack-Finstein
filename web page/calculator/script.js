38f6082ef8f2aa47853c5c0198a47e4fdf8c8683
Logesh1803-P <logesh.ucev@gamil.com>
2023-10-03 14:34:44 +0530

    oct 3

--- web page/calculator/script.js
+++ web page/calculator/script.js
@@ -1,19 +1,96 @@
 const display=document.getElementById("display")
 function appendToDisplay(char)
 {
-   
+    
     if(char == "AC")
         display.value =" ";
-    // else if(char=="=")
-    // display.value="logesh";
     else
+
       display.value += char;
 
-}
-
-function  appendToEval(){
-    const getValue = document.getElementById("display").value
-    const evaluate = eval(getValue)
-    document.getElementById("display").value = evaluate
 
 }
+
+// function  appendToEval(){
+//     const getValue = document.getElementById("display").value
+//     const evaluate = eval(getValue)
+//     document.getElementById("display").value = evaluate
+//     console.log(evaluate)
+// }
+
+function appendToEval(){
+    expression = document.getElementById("display").value;
+    // Add_patern = /+/g;
+    // calculate = getValue.match(/[+]/g);
+    // console.log(calculate);
+    function calculateExpression(expression) {
+        const operators = '+-*/';
+        const precedence = { '+': 1, '-': 1, '*': 2, '/': 2 };
+        const values = [];
+        const ops = [];
+      
+        expression = expression.replace(/ /g, ''); // Remove spaces
+      
+        let currentNum = '';
+    
+        for (const char of expression) {
+          if (!operators.includes(char)) {
+            currentNum += char;
+          } else {
+            if (currentNum !== '') {
+              values.push(parseFloat(currentNum));
+              currentNum = '';
+            }
+            while (
+              ops.length > 0 &&
+              precedence[ops[ops.length - 1]] >= precedence[char]
+            ) {
+              const operator = ops.pop();
+              const right = values.pop();
+              const left = values.pop();
+              values.push(applyOperator(left, right, operator));
+            }
+            ops.push(char);
+          }
+        }
+    
+    
+      
+        if (currentNum !== '') {
+          values.push(parseFloat(currentNum));
+        }
+      
+        while (ops.length > 0) {
+          const operator = ops.pop();
+          const right = values.pop();
+          const left = values.pop();
+          values.push(applyOperator(left, right, operator));
+        }
+      
+        return values[0];
+      }
+      
+    
+      function applyOperator(left, right, operator) {
+        switch (operator) {
+          case '+':
+            return left + right;
+          case '-':
+            return left - right;
+          case '*':
+            return left * right;
+          case '/':
+            if (right === 0) {
+              throw new Error('Division by zero');
+            }
+            return left / right;
+        }
+      }
+      
+      const result = calculateExpression(expression);
+      
+      console.log(result); // Output: 44
+      document.getElementById("display").value = result
+
+}
+
